cce64cadde39ba539b23bac76dd59f64bda125eb
Lukhasz <lukaskaarle@gmail.com>
2023-03-06 10:32:00 +0100

    Added Creation and Population scripts

---
+++ CreateDB.sql
@@ -0,0 +1,53 @@
+DROP DATABASE Streamflix;
+
+CREATE DATABASE Streamflix;
+
+USE Streamflix;
+
+CREATE TABLE titles (
+	id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR (255), 
+    year INT NOT NULL, 
+    PRIMARY KEY (id)
+    );
+    
+CREATE TABLE actors ( 
+	id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR (255), 
+    gender CHAR (1), 
+    PRIMARY KEY (id)
+    );
+    
+CREATE TABLE ratings ( 
+	id INT NOT NULL AUTO_INCREMENT, 
+    title_id INT, 
+    rating FLOAT (2,1), 
+    PRIMARY KEY (id), 
+    FOREIGN KEY (title_id) REFERENCES titles (id)
+    );
+    
+CREATE TABLE producers (
+	id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR (255), 
+    PRIMARY KEY (id)
+    );
+    
+CREATE TABLE title_producers (
+	title_id INT, 
+    producer_id INT, 
+    FOREIGN KEY (title_id) REFERENCES titles (id), 
+    FOREIGN KEY (producer_id) REFERENCES producers (id)
+    );
+
+
+
+
+
+
+
+
+SELECT * FROM titles;
+SELECT * FROM actors;
+SELECT * FROM ratings; 
+SELECT * FROM producers;
+SELECT * FROM title_producers;
